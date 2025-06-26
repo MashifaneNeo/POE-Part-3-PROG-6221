3f9d54edf5b33a304bacb5b08827ba96c209e844
MashifaneNeo <ST10449143@imconnect.edu.za>
2025-06-27 00:32:31 +0200

    Formatting

--- ST10449143_PROGPOEPART3/HelpMenu.cs
+++ ST10449143_PROGPOEPART3/HelpMenu.cs
@@ -9,19 +9,18 @@ namespace ST10449143_PROGPOEPART3
         {
             Brush green = Brushes.DarkGreen;
             output("╔═════════════════════════════════════════════════════════════════════╗", green);
-            output("║ You can ask me about the following cybersecurity topics:            ║", green);
-            output("║  - password, phishing, scam, privacy, encryption                    ║", green);
-            output("║  - firewall, antivirus, backup, two-factor authentication           ║", green);
-            output("║  - malware, vpn                                                     ║", green);
-            output("║                                                                     ║", green);
-            output("║ You can also tell me:                                               ║", green);
-            output("║  - How you feel about Cybersecurity like:                           ║", green);
-            output("║    worried, curious, frustrated, confused,                          ║", green);
-            output("║    thankful, anxious, excited, scared,                              ║", green);
-            output("║    uninterested and unsure                                          ║", green);
-            output("║  - Cybersecurity topics you're interested in                        ║", green);
-            output("║  - 'more', 'another', or 'explain' to get another tip on a topic    ║", green);
-            output("║  - 'exit' or 'quit' to close the chatbot                            ║", green);
+            output("║ You can ask me about the following cybersecurity topics:                                    ║", green);
+            output("║  - password, phishing, scam, privacy, encryption                                       ║", green);
+            output("║  - firewall, antivirus, backup, two-factor authentication                              ║", green);
+            output("║  - malware, vpn                                                                  ║", green);
+            output("║                                                                                     ║", green);
+            output("║ You can make use of new features such as                                            ║", green);
+            output("║  - Adding tasks                                                          ║", green);
+            output("║  - Setting reminders                                                        ║", green);
+            output("║  - Taking a quiz to test your Cybersecurity knowledge                              ║", green);
+            output("║  - Keep track of your scores and activites                                    ║", green);
+            output("║  - Ask the chatbot a wide variety of questions                                       ║", green);            
+            output("║  - 'exit' or 'quit' to close the chatbot                                                       ║", green);
             output("╚═════════════════════════════════════════════════════════════════════╝", green);
         }
     }
